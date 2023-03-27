711c83d1915f72ce4a8fd10bcd8d8cdcd6c6d842
Ezequiel <73133884+ezecastrog@users.noreply.github.com>
2023-03-27 14:58:35 -0300

    :sparkles: feature stats updated

--- scripts/main-stats.js
+++ scripts/main-stats.js
@@ -1,6 +1,8 @@
 let urlAPI = 'https://mindhub-xj03.onrender.com/api/amazing';
 let events = [];
+let pastEvents =[];
 let categories =[];
+let currentDate = '';
 const contenedorTabla = document.getElementById('table-stats');
 
 //Llamada de funciones
@@ -10,48 +12,108 @@ getEvents();
 
 //Funciones
 function loadStats(categorias){
-    let container = document.querySelector("tbody");
-    let tableBodyHTML = "";
+    let container = document.querySelector('tbody');
+    let firstTableHTML = `
+    <tr>
+      <td>Event with the highest percentage of attendance</td>
+      <td>Event with the lowest percentage of attendance</td>
+      <td>Event with larger capacity</td>
+    </tr>
+    `
+    let secondTableHTML = `
+    <tr>
+    <th scope="row" colspan=3 class="bg-dark-subtle">Upcoming events statistics by category</th>
+  </tr>
+  <tr >
+    <td>Cateogries</td>
+    <td>Revenues</td>
+    <td>Percentage of attendance</td>
+  </tr>
+    `
+    let thirdTableHTML = `
+    <tr>
+    <th scope="row" colspan="3" class="bg-dark-subtle">Past Events statistic by category</th>
+  </tr>
+  <tr>
+    <td>Cateogries</td>
+    <td>Revenues</td>
+    <td>Percentage of attendance</td>
+  </tr>
+  `
+    let tableEventStatisticsHTML = "";
+    let tablePastEventsHTML = "";
+    let tableUpcomingEventsHTML ="";
+    let pastEvents = extractPastEvents(events);
+    let masAsistido = getMostAssisted(pastEvents)
+    let porcentajeMasAsistido = Math.round((masAsistido.assistance/masAsistido.capacity)*100)
+    let menosAsistido = getLessAssisted(pastEvents)
+    let porcentajeMenosAsistido = Math.round((menosAsistido.assistance/menosAsistido.capacity)*100)
+    let masCapacidad = getMostCapacity(pastEvents);
+    tableEventStatisticsHTML +=`
+        <tr>
+        <td>${porcentajeMasAsistido}% (${masAsistido.name})</td>
+        <td>${porcentajeMenosAsistido}% (${menosAsistido.name})</td>
+        <td>${masCapacidad.capacity} (${masCapacidad.name})</td>
+        </tr>
+        `
+
     categorias.forEach(category=>{
         let filtradosEventos = extractEventByCategory(category,events);
-        let promedioAsistencia = getPromedioAsistencia(filtradosEventos);
-        let masAsistido = getMostAssisted(filtradosEventos);
-        let gananciasMasAsistido = masAsistido * category.capacity/100;
-        let menosAsistido = getLessAssisted(filtradosEventos);
-        tableBodyHTML += `<tr>
+
+        let upcomingEvents = extractUpcomingEvents(filtradosEventos);
+        let gananciaUpcomingEvents = getRevenue(upcomingEvents)
+        let porcentajeAsistenciaUpcomingEvents = getPercentageAssistance(upcomingEvents);
+
+        let pastEvents = extractPastEvents(filtradosEventos);
+        let gananciaPastEvents = getRevenue(pastEvents);
+        let porcentajeAsistenciaPastEvents= getPercentageAssistance(pastEvents);
+        
+        tableUpcomingEventsHTML +=`<tr>
         <td>${category}</td>
-        <td>${promedioAsistencia}</td>
-        <td>${masAsistido.assistance} (${masAsistido.name})</td>
-        <td>${menosAsistido.assistance} (${menosAsistido.name})</td>
-    </tr>
+        <td>$${gananciaUpcomingEvents}</td>
+        <td>${porcentajeAsistenciaUpcomingEvents}%</td>
+        </tr>
         `
-    })
-    container.innerHTML = tableBodyHTML;
+
+        tablePastEventsHTML +=`<tr>
+        <td>${category}</td>
+        <td>$${gananciaPastEvents}</td>
+        <td>${porcentajeAsistenciaPastEvents}%</td>
+        </tr>
+        `
+    })    
+    container.innerHTML = firstTableHTML += tableEventStatisticsHTML+=secondTableHTML +=tableUpcomingEventsHTML += thirdTableHTML += tablePastEventsHTML
+
+}
+function extractUpcomingEvents(events){
+    return events.filter(evento=>evento.date>currentDate);
+
+}
+function extractPastEvents(events){
+    return events.filter(evento=>evento.date<currentDate);
 }
 
-
-
-
 function extractCategories(arrayEventos){
-    let categorias=[];
     let categoriasRepetidas = arrayEventos.map(eventos => eventos.category); 
     categories = new Set(categoriasRepetidas)
-    console.log(categories)
 }
 
 function extractEventByCategory(category,events){
 return events.filter(evento=>evento.category.includes(category));
 }
-function getPromedioAsistencia(eventos){
-    let sumaAsistencia = 0;
-    eventos.forEach(evento=>sumaAsistencia += evento.assistance|| evento.estimate);
-    return Math.round(sumaAsistencia/eventos.length)
+
+function getPercentageAssistance(eventos){
+    let sumAssistance = 0;
+    eventos.forEach(evento=>sumAssistance += ((evento.assistance||evento.estimate)/evento.capacity)*100 )
+    return Math.round(sumAssistance/eventos.length)
 }
-// function getPromedioEstimado(eventos){
-    // let sumaEstimado = 0;
-    // eventos.forEach(evento=>sumaEstimado += evento.estimate);
-    // return Math.round(sumaEstimado/eventos.length);
-// }
+
+function getRevenue(eventos){
+    let sumRevenue = 0;
+    eventos.forEach(evento=>sumRevenue +=((evento.price)*(evento.assistance||evento.estimate)))
+    return Math.round(sumRevenue)
+}
+
 function getMostAssisted(events){
     return events.reduce((acumulador,valorActual)=>{
         if (valorActual.assistance>acumulador.assistance){
@@ -70,20 +132,27 @@ function getLessAssisted(events){
         }
     })
 }
+function getMostCapacity(events){
+    return events.reduce((acumulador,valorActual)=>{
+        if (valorActual.capacity>acumulador.capacity){
+            return valorActual;
+        } else {
+            return acumulador;
+        }
+    })
 
-
+}
 async function getEvents () {
     try {
         let response = await fetch(urlAPI);
         let dataAPI = await response.json();
-        console.log(dataAPI)
         events = dataAPI.events;
         currentDate = dataAPI.currentDate;
         categories = extractCategories(events);
-        console.log(categories);
-extractCategories(events);
-loadStats(categories);
+        extractCategories(events);
+        loadStats(categories);
 
 } catch (error){
         console.log(error.message);
-    }}
+    }
+}
